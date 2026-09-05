f4a9cbf7c193bccf37f5545305d6185f5fb85430
kquinillap-umg <kevinquinilla1@gmail.com>
2026-09-05 06:54:24 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kquinillap-umg.github.io',
-  base: '/GTA-VI',
   vite: {
     plugins: [tailwindcss()],
   },
